df2eafd96fc17959cf8323c2fd9c6e0dc439dddb
rod773 <rodrigoarcadio1@gmail.com>
2024-10-21 16:31:56 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {createTheme,ThemeProvider} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const theme = createTheme({
+
+
+});
+
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 const Home = () => {
   return (
     <>
-      <div style={{ height: "100%", width: "100%" }}>
+      <div style={{ height: "100%", width: "100%", textAlign: "center" }}>
         <h1>Home</h1>
+        <Button variant="contained">My Button</Button>
       </div>
     </>
   );
